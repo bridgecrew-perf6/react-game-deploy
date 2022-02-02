282cd64e46d2bb07cf2de6a6ea17eea2fc86cf68
manpacheco97 <manuel.pacheco@decemberlabs.com>
2022-02-02 13:44:08 -0300

    Updates

---
+++ precache-manifest.c87ec69ab0aa3886bf09889b1fc22697.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5c5f68750d12caea404f84a924c0dd52",
+    "url": "/react-game-deploy/index.html"
+  },
+  {
+    "revision": "b6b58336ff39adf864c6",
+    "url": "/react-game-deploy/static/css/main.e9acda51.chunk.css"
+  },
+  {
+    "revision": "cc0e9e5cb4b805f12eb1",
+    "url": "/react-game-deploy/static/js/2.96640ceb.chunk.js"
+  },
+  {
+    "revision": "2ded26446bc94b81347a",
+    "url": "/react-game-deploy/static/js/3.398cb210.chunk.js"
+  },
+  {
+    "revision": "b6b58336ff39adf864c6",
+    "url": "/react-game-deploy/static/js/main.4cc565d6.chunk.js"
+  },
+  {
+    "revision": "8224d900092d528c4a46",
+    "url": "/react-game-deploy/static/js/runtime~main.e9ae5f5f.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/react-game-deploy/static/media/logo.06e73328.svg"
+  }
+]);
